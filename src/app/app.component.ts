ec7a0e1d4360634a9d99ec6a2f2618b7527ea9bd
artesgo <artesgo@gmail.com>
2023-08-23 01:17:24 -0400

    setup some simple form controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'org';
+  builder = inject(FormBuilder);
+  form: FormGroup;
+
+  constructor() {
+    this.form = this.builder.group({
+      profile: this.builder.group({
+        name: new FormControl('Jinzo'),
+        quote: new FormControl(`It'll be done when it's done!`)
+      }),
+      projects: this.builder.array([
+        this.builder.group({
+          name: new FormControl('Music Notation'),
+          start: new FormControl('July 2020'),
+        }),
+        this.builder.group({
+          name: new FormControl('Grocery Aisle Mapper'),
+          start: new FormControl('Sept 2022'),
+        }),
+      ])
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, ProfileComponent],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
