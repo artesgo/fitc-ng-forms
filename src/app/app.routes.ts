a490ef585c4d47ef30d0a95169fd6bb95a6bc003
artesgo <artesgo@gmail.com>
2023-08-23 01:03:51 -0400

    setup profile component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Route } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const appRoutes: Route[] = [
-  { path: '', component: NxWelcomeComponent }
+  { path: '', component: ProfileComponent }
 ];
